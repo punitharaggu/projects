0273c427fa78f17293b50ea7c552cbdbda700289
punitharaggu <punithamonisha@gmail.com>
2019-04-22 19:39:15 +0200

    Calculator

---
+++ Arith_calc/src/components/ResultDis.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class ResultDis extends Component {
+
+
+    render() {
+        let {result} = this.props;
+        return (
+            <div className="result">
+                <p>{result}</p>
+            </div>
+    )
+        ;
+    }
+}
+
+
+export default ResultDis;
